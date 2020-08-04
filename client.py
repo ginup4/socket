f7810cfc56c5b47a7fd691f3b34502461510721c
Wiktor Pańczak <68958610+Wkitor@users.noreply.github.com>
2020-08-04 11:37:57 +0200

    test

--- client.py
+++ client.py
@@ -1,1 +1,25 @@
 import socket
+
+HEADER = 2
+PORT = 5049
+IP = "192.168.0.94"
+FORMAT = "utf-8"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def send(msg):
+	message = msg.encode(FORMAT)
+	msg_length = len(message).to_bytes(2, byteorder = "big")
+
+	client.send(msg_length)
+	client.send(message)
+
+def disconnect():
+	client.send(b"/xff/xff")
+
+def main():
+	client.connect((IP, P))
+	send("Hello World!")
+
+if __name__ == "__main__":
+	main()
--- server.py
+++ server.py
@@ -1,2 +1,29 @@
 import socket
 import threading
+
+PORT = 5049
+IP = socket.gethostbyname(socket.gethostname())
+HEADER = 2
+FORMAT = "utf-8"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def handle_client(conn, addr):
+	while True:
+		header = conn.recv(HEADER)
+		if header:
+			if header == b"/xff/xff":
+				print(f"{addr} disconnected")
+			msg_length = int.from_bytes(header[0:2], byteorder = "big")
+			msg = conn.recv(msg_length).decode(FORMAT)
+			print(f"<{addr}> {msg}")
+	conn.close()
+
+def main():
+	server.bind((IP, PORT))
+	server.listen()
+	while True:
+		thread = threading.Thread(target = handle_client, args = server.accept())
+
+if __name__ == "__main__":
+	main()
